625bd8459d7d26dec2b096d76508263065818c52
Stefan Hajnoczi <stefanha@gmail.com>
2016-12-09 09:43:01 +0000

    Add setup.py
    It is now possible to do:
    
      pip install --user .
    
    Signed-off-by: Stefan Hajnoczi <stefanha@gmail.com>

---
+++ setup.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='git-publish',
+      version='1.1.0',
+      description='Patch series management tool for git',
+      long_description='Prepare and store patch series as git tags',
+      url='https://github.com/stefanha/git-publish/',
+      author='Stefan Hajnoczi',
+      author_email='stefanha@gmail.com',
+      license='MIT',
+      classifiers=[
+          'Development Status :: 5 - Production/Stable',
+          'Environment :: Console',
+          'Intended Audience :: Developers',
+          'License :: OSI Approved :: MIT License',
+          'Programming Language :: Python :: 2',
+          'Topic :: Software Development',
+      ],
+      keywords='git patch series management',
+      scripts=['git-publish'],
+)
